dfca13e0b1cb23931973bf25583dd5b6682a7287
leahincom <leahincom@gmail.com>
2021-03-08 17:14:19 +0900

    add styles to ItemDetails page

--- client/src/component/ItemDetails.js
+++ client/src/component/ItemDetails.js
@@ -5,7 +5,7 @@ import ARTWORKS from '../db.json';
 import ARTSYS from '../artsy.json';
 import { useDropzone } from 'react-dropzone';
 import apiService from '../ApiService.js'
-      
+
 const ItemDetails = ({ match }) => {
   
   const [ item, setItem ] = useState({});
@@ -25,15 +25,31 @@ const ItemDetails = ({ match }) => {
 
   useEffect(() => {
     getItem();
-  }, []);
+    // setUpload(acceptedFiles);
+    upload.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [upload]);
 
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      setUpload(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+    }
+  });
 
   const files = acceptedFiles.map(file => (
     <li key={file.path}>
-      ({file.path} - {file.size} bytes)
+      {file.path}
     </li>
   ));
+
+  const thumbs = upload.map(file => (
+    <img
+      src={file.preview}
+      width='50%'
+    />
+));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -44,10 +60,6 @@ const ItemDetails = ({ match }) => {
     }
   }
 
-  const handleChange = ({ target }) => {
-    setUpload(target.value);
-  }
-  
   return (
     <div className='item-detail'>
 
@@ -56,37 +68,57 @@ const ItemDetails = ({ match }) => {
       and show the result on the right side 
       with 'save' button */}
 
-      <div className='input-images hidden'>
-        <img src='' alt='user_img' />
-        <img src='' alt='refer_img' />
-      </div>
-      
-
       <form onSubmit={handleSubmit} className='form'>
 
-        <section className="upload-container">
-          <div {...getRootProps({className: 'dropzone'})}>
+      <div className='images'>
 
-            {/* just show */}
-            <img src={item.thumbnail} width='400px' alt='artwork' />
+        <div className='input-imgs'>
 
-            {/* if mouse hover */}
-            <div className='drag-n-drop'>
-              <input {...getInputProps()} type="file" name="user_img" id="user_img" value={upload} onChange={handleChange} />
-              <div className='desc-shown'>
-                <strong>Choose a file<br /></strong>
-                <span class='box_dragndrop'> or drag it here</span>
+          <div className='selected_img'>
+            {thumbs}
+            <ul>{files}</ul>
+          </div>
+
+          <div className="upload-container">
+            <div {...getRootProps({className: 'dropzone'})}>
+
+              {/* just show */}
+              {/* first: big -> img upload: 50% */}
+              <img src={item.thumbnail} alt='artwork' />
+
+              {/* if mouse hover */}
+              <div className='drag-n-drop'>
+                <input {...getInputProps()} name="user_img" id="user_img" />
+                <div className='desc-shown'>
+                {
+                  isDragActive ?
+                  <p>
+                    <strong>Drop the files here...</strong>
+                  </p> :
+                  <p>
+                    <strong>Choose a file<br /></strong>
+                    or drag it here
+                  </p>
+                }
+                </div>
+
               </div>
 
-              {/* when uploaded, show the file name with background image opacity=0.1 */}
-              <ul>{files}</ul>
+              {/* when loading (training) => loading image / if possible) training images in video */}
+
             </div>
 
-            {/* when loading (training) => loading image / if possible) training images in video */}
-
           </div>
-          <button type='submit' className='formSubmit'>Upload</button>
-        </section>    
+
+        </div>
+
+        <div className='result_img'>
+          <img src='https://64.media.tumblr.com/71d4d7130532a10d7c9d6341fdc0a1f4/tumblr_ny8acwgw8u1qav3uso3_r1_540.gifv' />
+        </div>
+
+      </div>
+
+      <button type='submit' className='formSubmit'>Upload</button>
 
       </form>
       {/* upload usr img onto the style img */}
